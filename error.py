e78a812c03fdd66a9adea94112efb60a47ff4e97
NORCIM <96706271+upside-down588@users.noreply.github.com>
2022-03-18 23:26:19 +0600

    Add files via upload

---
+++ error.py
@@ -0,0 +1,14 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    os.system('pip install bs4')
+    os.system('pip install futures')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    __import__('error').check_subscription()
+elif bit == '32bit':
+    __import__('error').check_subscription()
